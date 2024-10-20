492048a22e598d5a34aaf7a10ebcbee913724854
b.smirnov <besm1@yandex.ru>
2024-10-20 23:47:10 +0300

    tyres_from_market -- HTMLParser

--- module_11_1_2.py
+++ module_11_1_2.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         #     mtrx[2000+i] = np.append(mtrx[2000+i], lines[j].split(';'), axis=0)
         # mtrx[i] = np.genfromtxt(fname=f'.\\Data\\col_zakdog{i}.csv',dtype=int,delimiter=';',)
         df[i] = pd.read_csv(filepath_or_buffer=f'.\\Data\\col_zakdog{i}.csv',delimiter=';'
-                              , encoding='cp1251', dtype={0:str},index_col=0,)
+                              , encoding='cp1251', dtype={0:str},index_col=0)
         # mtrx[i] = df[i].values
         # print(f'{i}, {mtrx[i]}')
         if len(cols) < len(df[i].columns):
@@ -35,8 +35,11 @@ gr = {}
 for i in idxs:
     gr[i] = pd.DataFrame(index=range(2021,2025),columns=cols, dtype=int)
     for y in range(2021,2025):
-        gr[i][y] = df[y].loc[i]
-    print(f'Contract type: {i}')
+        try:
+            gr[i].loc[y] = df[y].loc[i]
+        except KeyError :
+            pass
+    print(f'\nContract type: {i}')
     print(gr[i])
 # for i =
 # print(df[2021].columns)

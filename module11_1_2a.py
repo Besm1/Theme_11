2168b0ecade485d9afea6647d12d606a892af38b
b.smirnov <besm1@yandex.ru>
2024-10-22 13:05:42 +0300

    Contracts statistics_2

--- module11_1_2a.py
+++ module11_1_2a.py
@@ -1,13 +1,33 @@
-import numpy as np
+""""
+Задача.
+Имеются файлы типа MS Excel с годовыми отчётами некой коммерческой  медицинской организации по помесячному количеству
+заключённых договоров в разрезе их типов:
+
+        янв фев мар апр ....
+  тип1  n11 n12 n13 n14 ...
+  тип2  n21 n22 n23 n24 ...
+   ...  ... ... ... ... ...
+  ИТОГО T1 T2  T3  T4   ...
+
+Необходимо собрать данные со всех имеющихся файлов по трём типам договоров (ведение родов, ведение беременности, детские
+и госпитализация). В результате надо получить три графика, по одному на каждый из трёх типов договоров. Ось ординат - года,
+ось абсцисс - суммарное количество заключенных договоров за весь период с января по последний месяц в самом "коротком"
+файле.
+("Самым коротким" может быть файл за текущий год, т.к. в нём в общем случае присутствуют данные только с января
+по прошлый (полностью завершённый) месяц. Таким образом на графиках будет наглядно видна тенденция изменения количества
+договоров за аналогичные периоды лет, представленных в отчётах.)
+
+Файлы имеют наименования col_zakdogNNNN.xlsx, где NNNN - четырёхзначный номер года, за который представлен отчёт.
+Количество файлов и годы, за которые представлены отчёты, заранее не известны, известно только, что медицинское
+учреждение на рынке с 2011 года.
+
+"""
+
 import pandas as pd
-from pandas.core.dtypes.missing import na_value_for_dtype
-from pandas.core.interchange.dataframe_protocol import DataFrame
 import matplotlib.pyplot as plt
-import seaborn as sns
-from pandas.core.reshape.reshape import stack
 
 YEAR_1ST = 2018
-YEAR_LAST = 2025
+YEAR_LAST = 2024 + 1
 
 def get_key(t_:tuple):
     return t_[1]
@@ -19,14 +39,13 @@ if __name__ == '__main__':
     idxs = []
     for i in range(YEAR_1ST, YEAR_LAST):
         with pd.ExcelFile(f'.\\Data\\col_zakdog{i}.xlsx') as xls:
-           df[i] = pd.read_excel(xls, xls.sheet_names[0], index_col=(0,1), header=5, skipfooter=1
-                                    , usecols=[k for k in range(0,11)])
-        print(f'Год: {i}')
-        print(df[i][:10])
-        if len(cols) < len(df[i].columns):
+            df[i] = pd.read_excel(io=xls, sheet_name=xls.sheet_names[0], index_col=(0,1), header=5)
+        # print(f'Год: {i}')
+        # print(df[i])
+        if len(cols) == 0 or len(cols) > len(df[i].columns):
             cols = df[i].columns
-        # idxs = idxs.union(df[i].index)
         idxs.extend(df[i].index)
+
 
 idx = list(set([get_key(i_) for i_ in idxs ]))
 idxs = list(zip([pd.NA]*len(idx), idx))
@@ -35,12 +54,13 @@ gr = {} # fOR DIAGRAMS
 grs = {}
 
 for i in idxs:
-    if 'Роды' in i[1] or 'беременн' in i[1] or 'Госпитализ' in i[1]:
+    if i[1] in('Роды', 'Ведение беременности', 'Госпитализация', 'Детство'):
         gr[i] = pd.DataFrame(index=range(YEAR_1ST, YEAR_LAST),columns=cols, dtype=int)
         grs[i] = pd.DataFrame(index=range(YEAR_1ST, YEAR_LAST), columns=['Всего'], dtype=int)
         for y in range(YEAR_1ST, YEAR_LAST):
             try:
                 gr[i].loc[y] = df[y].astype(dtype=int,copy=True).loc[i]
+
                 grs[i].loc[y] = sum(df[y].astype(dtype=int,copy=True).loc[i])
             except KeyError :
                 pass

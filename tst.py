5c9cfcb7ea36beea5cadee739bab797ed2841b64
b.smirnov <besm1@yandex.ru>
2024-10-24 22:39:27 +0300

    Contracts statistics_2

--- tst.py
+++ tst.py
@@ -1,12 +1,25 @@
-import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
 import pandas as pd
 
-def repl(s:str):
-    return s.replace('\n','')
+YEAR_1ST = 2011 # Дата выхода организации на рынок
+
+def get_key(t_:tuple):
+    """
+    Для создания уникального набора двухуровневых индексов - вытаскивать только второй, значащий, уровень индекса
+    """
+    return t_[1]
 
 if __name__ == '__main__':
-    mtrx = {}
-    for i in range(2021, 2025):
-        mtrx[i] = pd.read_csv(filepath_or_buffer=f'.\\Data\\col_zakdog{i}.csv',delimiter=';'
-                              ,index_col=0, encoding='cp1251', dtype=int).values
-        print(f'{i}, {mtrx[i]}')
+    # df = {}     # Словарь DataFrame'ов: ключ - год, данные - DataFrame, считанный из файла
+    cols = []   # Названия колонок
+    idxs = []   # Уникальный набор индексов строк данных
+    year = YEAR_1ST     # Счётчик годов, начинается с первого года работы учреждения
+    year_cur = datetime.now().year      # Текущий год - последний из возможных отчётных
+    with pd.ExcelFile(f'.\\Data\\col_zakdog2020.xlsx') as xls:
+         df = pd.read_excel(io=xls, sheet_name=xls.sheet_names[0], header=5, index_col=0, usecols=lambda x: 'Unnamed' not in x,)
+    print(df.index)
+    print(df.columns)
+
+
+# usecols=lambda x: 'Unnamed' not in x,
